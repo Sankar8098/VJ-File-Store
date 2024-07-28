1b101b97c85c7f6a886b2103db2e42da5249d188
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-28 23:15:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,118 +1,84 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+import re
+import os
+from Script import script
 
+id_pattern = re.compile(r'^.\d+$')
 
-import sys
-import glob
-import importlib
-from pathlib import Path
-from pyrogram import idle
-import logging
-import logging.config
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+      
+# Bot Information
+API_ID = int(os.environ.get("API_ID", "23990433"))
+API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7450625675:AAEWPP-I4rQmM4Qr9HtlOkH9G5-ielfV1D0")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+PICS = os.environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg').split()  # Bot Start Picture
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '5821871362').split()]
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in os.environ.get('AUTH_CHANNEL', '-1001864683653').split()]  # give channel id with separate space. Ex : ('-10073828 -102782829 -1007282828')
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "File_store_streaming_bot")  # without @
+PORT = int(os.environ.get("PORT", "8080"))
 
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+# Clone Info:
+CLONE_MODE = is_enabled(os.environ.get('CLONE_MODE', "True"), True)  # Set True or False
+
+# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+CLONE_DB_URI = os.environ.get("CLONE_DB_URI", "mongodb+srv://clonefilestorebot:1234@clonefilestorebot.xuymndi.mongodb.net/?retryWrites=true&w=majority&appName=clonefilestorebot")
+CDB_NAME = os.environ.get("CDB_NAME", "clonetechvj")
+
+# Database Information
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Filestorebot:1234@filestorebot.egd2ux0.mongodb.net/?retryWrites=true&w=majority&appName=Filestorebot")
+DB_NAME = os.environ.get("DB_NAME", "techvj")
+
+# Auto Delete Information
+AUTO_DELETE_MODE = is_enabled(os.environ.get('AUTO_DELETE_MODE', "False"), False)  # Set True or False
+
+# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+AUTO_DELETE = int(os.environ.get("AUTO_DELETE", "30"))  # Time in Minutes
+AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "1800"))  # Time in Seconds
+
+# Channel Information
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374"))
+DEF_CAP = os.environ.get(
+    "DEF_CAP",
+    "<b><a href='telegram.me/SK_MoviesOffl'>{file_name} Telegram : @SK_MoviesOffl\n\nForward the file before Downloading.</a></b>"
 )
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# File Caption Information
+CUSTOM_FILE_CAPTION = os.environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+BATCH_FILE_CAPTION = os.environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
+# Enable - True or Disable - False
+PUBLIC_FILE_STORE = is_enabled(os.environ.get('PUBLIC_FILE_STORE', "True"), True)
 
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from config import LOG_CHANNEL, ON_HEROKU, CLONE_MODE, PORT
-from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
-from Script import script 
-from datetime import date, datetime 
-import pytz
-from aiohttp import web
-from TechVJ.server import web_server
+# Verify Info:
+VERIFY_MODE = is_enabled(os.environ.get('VERIFY_MODE', "False"), False)  # Set True or False
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")  # shortlink api
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")  # shortlink domain without https://
+VERIFY_TUTORIAL = os.environ.get("VERIFY_TUTORIAL", "")  # how to open link 
 
-import asyncio
-from pyrogram import idle
-from plugins.clone import restart_bots
-from TechVJ.bot import StreamBot
-from TechVJ.utils.keepalive import ping_server
-from TechVJ.bot.clients import initialize_clients
+# Website Info:
+WEBSITE_URL_MODE = is_enabled(os.environ.get('WEBSITE_URL_MODE', "False"), False)  # Set True or False
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+WEBSITE_URL = os.environ.get("WEBSITE_URL", "")  # For More Information Check Video On Yt - @Tech_VJ
 
+# File Stream Config
+STREAM_MODE = is_enabled(os.environ.get('STREAM_MODE', "True"), True)  # Set True or False
 
-ppath = "plugins/*.py"
-files = glob.glob(ppath)
-StreamBot.start()
-loop = asyncio.get_event_loop()
+# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(os.environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
-async def start():
-    print('\n')
-    print('Initalizing Tech VJ Bot')
-    bot_info = await StreamBot.get_me()
-    StreamBot.username = bot_info.username
-    await initialize_clients()
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem.replace(".py", "")
-            plugins_dir = Path(f"plugins/{plugin_name}.py")
-            import_path = "plugins.{}".format(plugin_name)
-            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-            load = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(load)
-            sys.modules["plugins." + plugin_name] = load
-            print("Tech VJ Imported => " + plugin_name)
-    if ON_HEROKU:
-        asyncio.create_task(ping_server())
-    me = await StreamBot.get_me()
-    tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    now = datetime.now(tz)
-    time = now.strftime("%H:%M:%S %p")
-    app = web.AppRunner(await web_server())
-    await StreamBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    if CLONE_MODE == True:
-        await restart_bots()
-    print("Bot Started Powered By @VJ_Botz")
-    await idle()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-if __name__ == '__main__':
-    try:
-        loop.run_until_complete(start())
-    except KeyboardInterrupt:
-        logging.info('Service Stopped Bye 👋')
-
+ON_HEROKU = 'DYNO' in os.environ
+URL = os.environ.get("URL", "https://file-store-bot-streaming-working.onrender.com/")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
